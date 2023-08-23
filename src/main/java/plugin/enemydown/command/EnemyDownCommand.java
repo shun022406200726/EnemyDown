df1279a094af2f94917e26ebac6019448b4bc874
Shun0224 <shun.085@gmail.com>
2023-08-23 17:42:09 +0900

    ゾンビスケルトン発生

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -1,14 +1,19 @@
 package plugin.enemydown.command;
 
+import java.util.List;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class EnemyDownCommand implements CommandExecutor {
 
@@ -16,23 +21,42 @@ public class EnemyDownCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player){
+
+      ItemStack customSword = new ItemStack(Material.DIAMOND_SWORD);
+      ItemMeta meta = customSword.getItemMeta();
+      meta.setDisplayName("最強の剣");
+      meta.addEnchant(Enchantment.DAMAGE_ALL, 10, true); // エンチャントを追加
+// 他のアイテムのカスタマイズ（属性やロアなど）も可能です
+      customSword.setItemMeta(meta);
+      player.getInventory().addItem(customSword);
+
+
+
+
+      World world=player.getWorld();
       player.setHealth(20);
       player.setFoodLevel(20);
 
-      World world=player.getWorld();
-      Location playerLocation=player.getLocation();
-      double x = playerLocation.getX();
-      double y = playerLocation.getY();
-      double z = playerLocation.getZ();
-
-      int random=new SplittableRandom().nextInt(20)-10;
-      world.spawnEntity(new Location(world,(x + random), y, (z + random)), EntityType.ZOMBIE);
-
-
+      world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
-
-
-
     return false;
+  }
+
+  private static Location getEnemySpawnLocation(Player player, World world) {
+    Location playerLocation= player.getLocation();
+    int randomX=new SplittableRandom().nextInt(20)-10;
+    int randomZ=new SplittableRandom().nextInt(20)-10;
+    double x = playerLocation.getX()+randomX;
+    double y = playerLocation.getY();
+    double z = playerLocation.getZ()+randomZ;
+
+    return new Location(world, x, y, z);
+  }
+
+  private static EntityType getEnemy() {
+    List<EntityType> enemyList=List.of(EntityType.ZOMBIE,EntityType.SKELETON);
+    int random=new SplittableRandom().nextInt(2);
+    EntityType enemy = enemyList.get(random);
+    return enemy;
   }
 }
